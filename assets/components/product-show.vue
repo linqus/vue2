adb17b64323be41a7e72a40898bca3a7c71104ed
Karol Linkiewicz <karol@linkiewicz.pl>
2023-09-26 21:46:53 +0200

    template and style in product show

--- assets/components/product-show.vue
+++ assets/components/product-show.vue
@@ -2,23 +2,69 @@
     <div>
         <loading v-if="loading" />
         <div v-if="product">
-            <title-component
-                :text="product.name"
-            />
+            <title-component :text="product.name" />
+        </div>
+        <div
+            v-if="product"
+            :class="$style.product"
+            class="row"
+        >
+            <div class="col-4 pt-3">
+                <img
+                    class="d-block"
+                    :src="product.image"
+                    :alt="product.name"
+                >
+                <div class="p-2">
+                    <small>brought to you by </small>
+                    <small
+                        class="d-inline"
+                        v-text="product.brand"
+                    />
+                </div>
+            </div>
+            <div class="col-8 p-3">
+                <div v-text="product.description" />
+                <div class="row mt-4 align-items-center">
+                    <div class="col-4">
+                        Price: <strong>${{ price }}</strong>
+                    </div>
+                    <div class="col-8 p-3">
+                        <div class="d-flex align-items-center justify-content-center">
+                            <color-selector
+                                v-if="product.colors.length !== 0"
+                            />
+                            <input
+                                class="form-control mx-3"
+                                type="number"
+                                min="1"
+                            >
+                            <button
+                                class="btn btn-info btn-sm"
+                            >
+                                Add to Cart
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import { fetchProductById } from '@/services/products-service';
+import formatPrice from '@/helpers/format-price';
 import Loading from '@/components/loading';
 import TitleComponent from '@/components/title';
+import ColorSelector from '@/components/color-selector';
 
 export default {
     name: 'ProductShow',
     components: {
         Loading,
         TitleComponent,
+        ColorSelector,
     },
     props: {
         currentProductId: {
@@ -32,14 +78,31 @@ export default {
             loading: true,
         };
     },
+    computed: {
+        price() {
+            return formatPrice(this.product.price);
+        },
+    },
     async created() {
         try {
             this.product = (await fetchProductById(this.currentProductId)).data;
-        } catch (e) {
-            console.log(e);
         } finally {
             this.loading = false;
         }
     },
 };
 </script>
+
+<style lang="scss" module>
+@import '~styles/components/light-component';
+.product {
+    @include light-component;
+    img {
+        max-width:100%;
+        max-height:100%;
+    }
+    input {
+        width: 60px;
+    }
+}
+</style>
