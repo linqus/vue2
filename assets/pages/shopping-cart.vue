f701123b70e2db0b65a1afac2d5db4fceac46ed4
Karol Linkiewicz <karol@linkiewicz.pl>
2023-10-02 21:27:37 +0200

    raw cart items

--- assets/pages/shopping-cart.vue
+++ assets/pages/shopping-cart.vue
@@ -5,9 +5,19 @@
 
             <div class="col-xs-12 col-lg-9">
                 <title-component text="Shopping Cart" />
-
+                <loading v-if="cart === null" />
                 <div class="content p-3">
-                    TODO - show the cart!
+                    <div v-if="cart !== null">
+                        <div
+                            v-for="(item, index) in cart.items"
+                            :key="index"
+                        >
+                            {{ item.product }} {{ item.quantity }} {{ index }}
+                        </div>
+                        <div v-if="cart.items.length === 0">
+                            Do some shopping!
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -17,15 +27,20 @@
 <script>
 import TitleComponent from '@/components/title';
 import ShoppingCartMixin from '@/mixins/get-shopping-cart.js';
+import Loading from '@/components/loading';
 
 export default {
     name: 'ShoppingCart',
     components: {
         TitleComponent,
+        Loading,
     },
     mixins: [
         ShoppingCartMixin,
-    ]
+    ],
+    data() {
+        return { loading: true };
+    },
 };
 </script>
 
