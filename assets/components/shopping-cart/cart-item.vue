cf5866ef33a2de6fe160df842537a0c0d2fae769
Karol Linkiewicz <karol@linkiewicz.pl>
2023-10-03 15:40:04 +0200

    cart template, formatPrice and totalPrice

--- assets/components/shopping-cart/cart-item.vue
+++ assets/components/shopping-cart/cart-item.vue
@@ -1,16 +1,32 @@
 <template>
     <div :class="[$style.component, 'row', 'p-3']">
-        {{ item.product.name }}
+        <div class="col-3">
+            {{ item.product.name }}
+        </div>
+        <div class="col-3">
+            {{ item.quantity}}
+        </div>
+        <div class="col-3">
+            {{ formattedPrice(item.product.price) }}
+        </div>
+        <div class="col-3" />
     </div>
 </template>
 
 <script>
+import formatPrice from '@/helpers/format-price';
+
 export default {
     name: 'SoppingCartItem',
     props: {
         item: {
             type: Object,
             required: true,
+        },
+    },
+    methods: {
+        formattedPrice(price) {
+            return formatPrice(price);
         },
     },
 };
--- assets/components/shopping-cart/index.vue
+++ assets/components/shopping-cart/index.vue
@@ -3,16 +3,34 @@
         <div v-if="items.length === 0">
             Your cart is empty! Get to shopping!
         </div>
-        <shopping-cart-item
-            v-for="item in items"
-            :key="item.id"
-            :item="item"
-        />
+        <div v-if="items.length">
+            <div class="row p-3">
+                <div class="col-3">
+                    Item Name
+                </div>
+                <div class="col-3">
+                    Quantity
+                </div>
+                <div class="col-3">
+                    Price
+                </div>
+                <div class="col-3" />
+            </div>
+            <shopping-cart-item
+                v-for="item in items"
+                :key="item.id"
+                :item="item"
+            />
+            <div class="p-3">
+                Total: <strong>$ {{ totalPrice }}</strong>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import ShoppingCartItem from '@/components/shopping-cart/cart-item';
+import formatPrice from '@/helpers/format-price';
 
 export default {
     name: 'ShoppingCartList',
@@ -23,6 +41,17 @@ export default {
         items: {
             type: Array,
             required: true,
+        },
+    },
+    computed: {
+        totalPrice() {
+            let total = 0;
+
+            this.items.forEach((element) => {
+                total += element.product.price * element.quantity;
+            });
+
+            return formatPrice(total);
         },
     },
 };
